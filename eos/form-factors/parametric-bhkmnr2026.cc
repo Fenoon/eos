8c0377f3d709af0e97f6a4d7b090afaa952717f8
Fatemeh Nouri <fatemeh.nouri.7884@gmail.com>
2026-06-12 13:33:38 +0200

    [form-factors] Generalize the root penalty to the real axis of RS21

--- eos/form-factors/parametric-bhkmnr2026.cc
+++ eos/form-factors/parametric-bhkmnr2026.cc
@@ -536,7 +536,11 @@ namespace eos
             if (abs(r) < 1.0)
             {
                 const complex<double> phi_root = _chi_inverse(r, _s_to_phi_21(_s_m(), _s_in()), _s_to_phi_11(_s_0(), _s_in()));
-                if (std::real(phi_root) > 0.0 && std::abs(phi_root) < 1.0)
+                if (std::real(phi_root) > 0.0 && std::abs(phi_root) < 1.0 )
+                {
+                    penalty += 1.0 / std::abs(phi_root) - 1.0;
+                }
+                else if (std::imag(r) == 0.0) // Remove the zeros of the real axis of the 21 sheet since they are not physical
                 {
                     penalty += 1.0 / std::abs(phi_root) - 1.0;
                 }
